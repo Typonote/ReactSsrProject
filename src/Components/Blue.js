37419d84b4958dff8606401996eb1c8d2936c07a
JinKyung Kim <kimradium@naver.com>
2021-12-09 14:44:48 +0900

    #2 컴포넌트 만들기(p.541 ~)

---
+++ src/Components/Blue.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./scss/Blue.scss";
+
+const Blue = () => {
+  return <div className="Blue__Container">Blue</div>;
+};
+
+export default Blue;
---
+++ src/Components/Red.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./scss/Red.scss";
+
+const Red = () => {
+  return <div className="Red__Container">Red</div>;
+};
+
+export default Red;
